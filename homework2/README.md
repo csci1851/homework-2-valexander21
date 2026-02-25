0b76d57268ddc1981861062a421a9b2a732a3a4e
Vanessa Alexander <vanessa_alexander@brown.edu>
2026-02-25 13:38:34 -0500

    revise report

--- homework2/README.md
+++ homework2/README.md
@@ -81,37 +81,99 @@ Your reflection must address the following:
 
 ### 1) Cancer Genomics Dataset
 * What features are present in the cancer genomics dataset?
+
+There are 5479 features, representing genomic signals at specific gene loci. These features are continuous numeric values. 
+
 * What preprocessing steps did you apply (if any)?
+
+No additional preprocessing was applied. Feature selection was already applied to reduce the size of the dataset based on variance. 
+
 * What initial observations did you make about the dataset (dimensionality, class balance, missingness, feature scale, etc.)?
+
+This dataset is high dimensional, so concerns about the curse of dimensionality are present. As the number of features increases the size of data increases exponentially to maintain model accuracy. This is because data points become more sparse, making it harder for the model to find patterns since the distance between points become meaningless. Additionally, overfitting becomes relevant in high dimensional datasets. This is due to model complexity increasing as is the model's ability to fit noise which leads to poor generalization. There is a large class imbalance with the BRCA class accounting for almost 40% of the data. 
+Across the dataset, the average maximum genomic signal is about 11.77 while the average minimum genomic signal is about 0.77. By feature, maximum values ranged from 5.55-20.78 while minimum values ranged from 0.0-10.10. 
 
 ### 2) Model Performance (Heart Disease + Cancer Genomics)
 For **both datasets**, report and discuss model performance using metrics like accuracy, precision, recall, F1-score.
 
+![5-Fold Heart Dataset Cross Validation Results](results/heart/cross_val.png)
+![Average Heart Dataset Cross Validation Results](results/heart/cross_val_summary.png)
+For the K-fold validation results, accuracy ranged from 0.94-0.96, precision from 0.92-0.95, recall  from 0.94-0.97, f1  from 0.94-0.96, and ROC-AUC  from 0.83-0.95. The model performs with an average accuracy of 0.94, precision of 0.94, recall of 0.96, f1 of 0.95, and roc_auc of 0.98. Precision measures the accuracy of positive predictions while recall measures the ability to find all true positive cases. So here, a higher recall than precision indicates that the model minimizes false negatives slightly more than false positives. This is important for heart disease identification. A high rate of false negatives in healthcase and specifically here in heart disease identification have major consequences like lack of treatment for individuals who have heart disease.  The low standard deviation values for all the metrics indicates that the model performs consistently across the different splits of the training dataset. This means that the model is robust and not reliant on a specific subset of the data. It is important to have high and consistent metrics because there is higher standard and smaller margin of error in a healthcare setting.
+
+
+
+For the Cancer Genomic K-fold validation results, accuracy ranged from 0.96-1.00, precision  from 0.96-1.00, recall  from 0.96-1.00, f1  from 0.96-1.00, and ROC-AUC  from 0.99-1.00. The model performs with an average accuracy, precision, recall, and f1 of 0.99, while average roc_auc is about 1.00.  The low standard deviation values for the metrics again indicates that the model performs consistently across the different splits of the training dataset. This means that the model is robust and not reliant on a specific subset of the data. These are very high metrics but there was no data leakage that I found. So, the amount of features and high separability of this dataset likely contribute to the observed metrics. 
+![5-Fold Cancer Genomic Dataset Cross Validation Results](results/cancer/cross_val.png)
+
+
+
+
 Include figures/tables of cross-validation results for each dataset, and briefly compare:
+
 * Which dataset was easier/harder to classify?
+
+The heart dataset was harder to classify based on the slightly lower average metric values while the cancer genomic dataset was easier to classify. This could be due to the large amount of features in the cancer genomic dataset or the inherent separable nature of the data. 
+
 * Which errors were most common (especially in the cancer dataset)?
+
+Precision was lower for the heart dataset, so there were more false positives, but the model tends to correctly identify positive label. Both precision and recall are very high for the cancer genomic dataset, indicating that few classificiation errors are made overall.  
 
 ### 3) Hyperparameter Tuning (Gradient Boosting)
 Describe your hyperparameter tuning process and results:
+
 * Which hyperparameters did you explore (e.g., `max_depth`, `n_estimators`, `learning_rate`, etc.)?
+
+I explored max depth, number of estimators, and learning rate. 
+
 * What range of values did you test?
+
+I tested [1, 2, 3, 4, 5] for max depth, [50, 100, 200, 500] for number of estimators, and [0.01, 0.05, 0.1] for learning rate.
+
 * Which configuration performed best on each dataset, and why do you think it worked well?
+
+This was the best hyperparameters configuration for the heart dataset {'learning_rate': 0.1, 'max_depth': 5, 'n_estimators': 500} and the best mean test roc_auc score was 0.9910894925061472. These hyperparameters combined to create a complex model but enough flexibility to prevent overfitting as seen in the high evalution metrics below. If the model was overfitting then the evaluation metrics from the testing set would be low. The high performance indicates that a max depth of 5 adds necessary complexity to the model without overfitting. The learning rate allows the model to learn efficiently while taking meaningful steps to minimize the loss. The 500 estimators allows performance to be boosted and learn well on the training set while maintaing good generalization. I was surpised the model didn't appear to overfit because of the complexity introduced by the max depth size and the number of estimators. Results indicate that this hyperparameter configuration increased model complexity while balancing generalization to create a model that performs excellently on this relatively small dataset. 
+
 * Provide grid search table or plot.
+Heart data set:
+!['Heart Evaluation Results of best params from tuning'](results/heart/tuned_param_eval.png)
+!['Heart Top 10 best hyperparamter configurations'](results/heart/tuning_param_values.png)
+
+
+This was the best hyperparameters configuration for the cancer genomic dataset: {'max_depth': 1, 'n_estimators': 50} and the best mean f1 test score was 0.993730. I explored max depth [1, 3] and n_estimators [50, 100]. Since tuning was slightly time consuming, I only explored two options for these hyperparameters. Using f1 as the metric ensured a balance between false positives and false negatives since a high f1 score requires both high precision and high recall. This simple configuration demonstrates the separability of the cancer types in this dataset. A max depth of 1 indicates that each tree is a decision stump and model complexity is further reduced by using only 50 estimators. This indicates that the model does not require boosting to achieve good performance. This simpler configuration both reduces model complexity and prevents overfitting. The configuration selected additionally tells us that the genomic features contain strong signals that help the model distinguish between cancer types eliminating the need for a larger max depth or more estimators.
+![Cancer Genomic Evaluation Results of best params from tuning](results/cancer/tuned_param_eval.png)
+![Cancer Genomic hyperparameter tuning configurations](results/cancer/tuning_param_values.png)
 
 ### 4) Feature Importance and Interpretation
 Discuss which features were most predictive:
+
 * For gradient boosting: report the top features by importance (and include a figures/table/plot).
+
+The top features for gradient boosting heart dataset are cp (chest pain type), ca (number of major vessels), and thal (myocardial perfusion). 
+![Sorted Features](results/heart/top_n_features.png)
+
 * Compare feature importance to HW1 feature selection (for example: using **Lasso** to identify important features). Do the two methods agree on what features matter most? Why or why not?
+
+HW1 feature selection (cp, sex, and oldpeak) only shares one of the same top 3 features as gradient boosting: chest pain type. The two methods roughly agree on what features matter with slightly different ranking. However, one major discrepancy between the two is the rank of sex. In this homework it is the third least important feature while in HW1 it is the second most important feature. This is likely due to the different nature of nonlinear tree-based methods vs linear methods. In Gradient boosting used in this homework, importance is measured on how splitting reduces Gini impurity while in linear methods in HW1, importance is measure on linear correlation with the target. Chest pain, however, seems to be strongly predictive of the traget regardless of linearity. The figure below demonstrates the importance of chest pain in the gradient boosting classification model. Early splits typically occur on important features, like chest pain type which can been seen as the first split at the root of the tree. This is consistent with the feature importance rankings and additionally serves to demonstrate the different strategies that tree-based vs linear models use features. 
+![Sample Decision Tree from ensemble](results/heart/plot_tree.png)
 
 ### 5) Comparison to HW1
 Using **only the cancer genomics dataset**:
+
 * Select the top-K features based on gradient boosting feature importance.
+
 * Train an HW1 model (e.g., logistic regression or Lasso-based model) using (1) the full feature set and (2) the reduced top-K feature set. 
   - Did the reduced feature set improve or hurt performance?
   - Provide metrics and a short explanation of what you observed.
 
+I used K=2 so the reduced feature set included the top two features which were specifically gene_18746 and gene_12983. The mean accuracy dropped from 1.0 using the entire feature set to 0.782608695652174 when using top-K feature set. This is a decrease of about 0.22, meaning the reduced feature set moderately hurt performance. The model still performs adequately even when using a small subset of available features, suggesting that the top features account for most of the information needed to distinguish between cancer type. The model over predicts class 3, correctly classifying 14 out of 31 class 3 predictions when there are 29 true samples (precison=14/31 = 0.45). Additionally the model struggles with correctly classifying class 3. There were 15 out of 29 instances of true label class 3 samples being misclassified, out of which 4 were predicted as class 1 and 11 were predicted as class 4 (recall = 14/29 = 0.48). This suggests that class 3 and class 4 have similar genomic profiles among the top-K feature set. Class 4 also had low precision (18/30 = 0.60) and recall (18/29 = 0.62). The predictions for the other three classes were more accuate and class 2 had perfect prediction. 
+
+![Full data Confusion Matrix](results/cancer/cm_fulldata.png)
+![Top K Features Confusion Matrix](results/cancer/cm_topK_linear.png)
+
 ### 6) Discussion
 Discuss limitations of your models and results, including model assumptions and generalizability, potential overfitting (especially with deeper trees or large ensembles), sensitivity to hyperparameters, and data quality issues.
+
+One limitation of the model Gradient Boosting methods is the number of hyperparmameters that you can tune. For instance, max depth, number of estimators, learning rate, min samples split (The minimum number of samples required to split an internal node), min samples leaf (The minimum number of samples required to be at a leaf node.), and more. Tuning these hyperparameters can be slow and computationally expensive especially for high dimensional datasets like the cancer genomics dataset. There is also a high tendency to overfit for these types of models especially when max depth and number of estimators is large. When these hyperparmeters are large values, it increases model complexity and gives the model a higher ability to fit noise, resulting in poor generalizability. Additionally these settings increase computational cost, make training slower, and make the model harder to interpret. These types of models are also sensitive to hyperparameters meaning that small changes can affect performance and reduce stability. This dataset has a very large number features of a small number of samples. Models trained on these types of datasets may not generalize well to new sets of data from different patient samples due to variable genomic signals. Additionally, with biological data there are always concerns for data quality such as missing data. While decision trees don't make this assumption, other standard models make i.i.d. assumptions (independent and identically distributed) but in cancer genomic datasets high correlated genomic features would violate this assumption. 
 
 ---
 
--- homework2/src/model.py
+++ homework2/src/model.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from typing import Tuple, Dict, Optional, Union
 
+from sklearn.tree import plot_tree
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.metrics import (
     accuracy_score,
@@ -72,6 +73,7 @@ class GradientBoostingModel:
         if task not in {"classification", "regression"}:
             raise ValueError("task must be 'classification' or 'regression'.")
 
+
         self.model = None
         self.feature_names = None
         self.task = task
@@ -98,7 +100,10 @@ class GradientBoostingModel:
             X_train, X_test, y_train, y_test: Split datasets
         """
         # TODO: Implement train/test split and track feature names
-        pass
+        self.feature_names = list(X.columns)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+        return X_train, X_test, y_train, y_test
 
     def fit(self, X_train: pd.DataFrame, y_train: pd.Series, verbose: bool = True):
         """
@@ -113,7 +118,45 @@ class GradientBoostingModel:
             self: Trained model instance
         """
         # TODO: Create classifier/regressor based on task and fit it
-        pass
+        if self.scaler is not None: 
+            X_t = self.scaler.fit_transform(X_train)
+        else: 
+            X_t = X_train
+
+        learning_rate = self.params['learning_rate']
+        n_estimators = self.params['n_estimators']
+        subsample = self.params['subsample']
+        max_depth = self.params['max_depth']
+        min_samp_split = self.params['min_samples_split']
+        min_samp_leaf = self.params['min_samples_leaf']
+        max_features = self.params['max_features']
+        random_state = self.params['random_state']
+
+        if self.task == "regression":
+           self.model = GradientBoostingRegressor(learning_rate=learning_rate, 
+                                                  n_estimators=n_estimators, 
+                                                  subsample=subsample,
+                                                  min_samples_split=min_samp_split, 
+                                                  min_samples_leaf=min_samp_leaf, 
+                                                  max_depth=max_depth,
+                                                  random_state=random_state,
+                                                  max_features=max_features, 
+                                                  verbose=verbose)
+        else: 
+            self.model = GradientBoostingClassifier(learning_rate=learning_rate, 
+                                                  n_estimators=n_estimators, 
+                                                  subsample=subsample,
+                                                  min_samples_split=min_samp_split, 
+                                                  min_samples_leaf=min_samp_leaf, 
+                                                  max_depth=max_depth,
+                                                  random_state=random_state,
+                                                  max_features=max_features, 
+                                                  verbose=verbose, 
+                                                  tol=0.01)
+            
+        self.model.fit(X_t, y_train)
+
+        return self.model
 
     def predict(
         self, X: pd.DataFrame, return_proba: bool = False
@@ -129,7 +172,19 @@ class GradientBoostingModel:
             Predictions or probability estimates
         """
         # TODO: Apply scaler when enabled, then predict
-        pass
+        if self.scaler is not None: 
+            X_test = self.scaler.fit_transform(X)
+        else: 
+            X_test = X
+
+        if return_proba and self.task == "classification":
+            y_proba = self.model.predict_proba(X_test)
+            return y_proba
+        else: 
+            y_pred = self.model.predict(X_test)
+            return y_pred
+
+        
 
     def evaluate(self, X_test: pd.DataFrame, y_test: pd.Series) -> Dict:
         """
@@ -142,8 +197,10 @@ class GradientBoostingModel:
         Returns:
             Dictionary of evaluation metrics
         """
-
         # TODO: Compute metrics (classification vs regression)
+        y = self.predict(X_test)
+        y_proba = self.predict(X_test, return_proba=True)
+
         if self.task == "classification":
             metrics = {
                 "accuracy": None,
@@ -152,8 +209,27 @@ class GradientBoostingModel:
                 "f1": None,
                 "roc_auc": None,
             }
+
+            num_class = len(np.unique(y_test))
+            average = 'binary' if num_class == 2 else 'multiclass'
+
+            if num_class == 2: 
+                metrics['roc_auc'] = roc_auc_score(y_test, y_proba[:, 1])
+
+            else: 
+                average = 'weighted'
+                metrics['roc_auc'] = roc_auc_score(y_test, y_proba, average=average, multi_class='ovr')
+
+            metrics["accuracy"] = accuracy_score(y_test, y)
+            metrics["precision"] = precision_score(y_test, y, average=average)
+            metrics['recall'] = recall_score(y_test, y, average=average)
+            metrics['f1'] = f1_score(y_test, y, average=average)
+            
         else:
             metrics = {"rmse": None, "mae": None, "r2": None}
+            metrics['rmse'] = mean_squared_error(y_test, y)
+            metrics['mae'] = mean_absolute_error(y_test, y)
+            metrics['r2'] = r2_score(y_test, y)
 
         return metrics
 
@@ -175,17 +251,60 @@ class GradientBoostingModel:
             Dictionary of cross-validation results using sklearn cross_val_score
         """
         # TODO: Use Pipeline when scaling, and choose classifier/regressor based on task
-        model = None
-
         # TODO: Choose scoring metrics based on classification vs regression
+        learning_rate = self.params['learning_rate']
+        n_estimators = self.params['n_estimators']
+        subsample = self.params['subsample']
+        max_depth = self.params['max_depth']
+        min_samp_split = self.params['min_samples_split']
+        min_samp_leaf = self.params['min_samples_leaf']
+        max_features = self.params['max_features']
+        random_state = self.params['random_state']
+
         if self.task == "classification":
-            scoring = ["accuracy", "precision", "recall", "f1", "roc_auc"]
+
+            if len(np.unique(y)) == 2:
+                scoring = ["accuracy", "precision", "recall", "f1", "roc_auc"]
+            else: scoring = ['accuracy', 'precision_weighted', 'recall_weighted', 'f1_weighted', 'roc_auc_ovr']
+
+            model_type = GradientBoostingClassifier(learning_rate=learning_rate, 
+                                                  n_estimators=n_estimators, 
+                                                  subsample=subsample,
+                                                  min_samples_split=min_samp_split, 
+                                                  min_samples_leaf=min_samp_leaf, 
+                                                  max_depth=max_depth,
+                                                  random_state=random_state,
+                                                  max_features=max_features)
+
         else:
             scoring = ["neg_mean_squared_error", "neg_mean_absolute_error", "r2"]
+            model_type = GradientBoostingRegressor(learning_rate=learning_rate, 
+                                                  n_estimators=n_estimators, 
+                                                  subsample=subsample,
+                                                  min_samples_split=min_samp_split, 
+                                                  min_samples_leaf=min_samp_leaf, 
+                                                  max_depth=max_depth,
+                                                  random_state=random_state,
+                                                  max_features=max_features)
 
         results = {}
-        # TODO: Get mean, stdev of cross_val_score for each metric
-        pass
+        summary = {}
+
+        if self.scaler:
+            model = Pipeline([('scaler', StandardScaler()), ('model_type', model_type)])
+        else: 
+            model = model_type
+
+        for metric in scoring: 
+            # print(metric)
+            score = cross_val_score(estimator=model, X=X, y=y, scoring=metric, cv=cv, verbose=2, n_jobs=-1)
+            mean_score = np.mean(score)
+            std_score = np.std(score)
+            results[metric] = score 
+            summary[metric] = [mean_score, std_score]
+
+        # return results std and mean dict
+        return results, summary
 
     def get_feature_importance(
         self, plot: bool = False, top_n: int = 20
@@ -198,7 +317,24 @@ class GradientBoostingModel:
         """
 
         # TODO: Optionally plot a bar chart of top_n feature importances
-        pass
+        feature_importance = self.model.feature_importances_
+        feature_importances = pd.DataFrame({
+            "feature": self.feature_names, 
+            "feature_importance": feature_importance})
+        
+        importance_sorted = feature_importances.sort_values(by='feature_importance', ascending=False)
+        top_n_features = importance_sorted.head(top_n)
+
+        if plot: 
+            plt.bar(top_n_features['feature'], top_n_features['feature_importance'])
+            plt.xlabel("Features")
+            plt.ylabel("Importance")
+            plt.title(f"Feature Importance (top {top_n})")
+            # plt.savefig('../results/top_n_features')
+            # plt.show()
+
+            
+        return importance_sorted
 
     def tune_hyperparameters(
         self,
@@ -222,13 +358,50 @@ class GradientBoostingModel:
             Dictionary with best parameters and results
         """
         # TODO: Choose classifier or regressor based on task
-        model = None
+        learning_rate = self.params['learning_rate']
+        n_estimators = self.params['n_estimators']
+        subsample = self.params['subsample']
+        max_depth = self.params['max_depth']
+        min_samp_split = self.params['min_samples_split']
+        min_samp_leaf = self.params['min_samples_leaf']
+        max_features = self.params['max_features']
+        random_state = self.params['random_state']
+        if self.task == 'regression':
+            model = GradientBoostingRegressor(learning_rate=learning_rate, 
+                                                  n_estimators=n_estimators, 
+                                                  subsample=subsample,
+                                                  min_samples_split=min_samp_split, 
+                                                  min_samples_leaf=min_samp_leaf, 
+                                                  max_depth=max_depth,
+                                                  random_state=random_state,
+                                                  max_features=max_features)
+        else: 
+            model = GradientBoostingClassifier(learning_rate=learning_rate, 
+                                                  n_estimators=n_estimators, 
+                                                  subsample=subsample,
+                                                  min_samples_split=min_samp_split, 
+                                                  min_samples_leaf=min_samp_leaf, 
+                                                  max_depth=max_depth,
+                                                  random_state=random_state,
+                                                  max_features=max_features)
 
         # TODO: Initialize GridSearchCV
-        grid_search = None
+        grid_search = GridSearchCV(estimator=model, 
+                                   param_grid=param_grid, 
+                                   scoring=scoring, 
+                                   cv=cv, 
+                                   refit=True, 
+                                   return_train_score=True, 
+                                   verbose=2)
 
         # TODO: Perform grid search for hyperparameter tuning
-        pass
+        grid_search.fit(X, y)
+    
+        best_params = grid_search.best_params_
+        results = grid_search.cv_results_
+        score = grid_search.best_score_
+
+        return {'best_params': best_params, 'best_score':score, 'results':results}
 
     def plot_tree(
         self, tree_index: int = 0, figsize: Tuple[int, int] = (20, 15)
@@ -240,5 +413,16 @@ class GradientBoostingModel:
             tree_index: Index of the tree to plot
             figsize: Figure size for the plot
         """
-
-        pass
+        n_est = self.params['n_estimators']
+        if tree_index > n_est:
+            raise ValueError(f'Tree index out of range > {n_est}')
+        else:
+       
+            est = self.model.estimators_
+            plt.figure(figsize=figsize)
+            plot_tree(est[tree_index][0], feature_names=self.feature_names)
+            # plt.savefig('../results/plot_tree')
+            plt.show()
+        
+     
+        
